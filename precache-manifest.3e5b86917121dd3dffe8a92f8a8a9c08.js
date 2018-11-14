6f02a571afe0eec64231db4f266d1a4feca5152e
KN <knny.nguyen@gmail.com>
2018-11-14 13:17:47 -0500

    Updates

---
+++ precache-manifest.3e5b86917121dd3dffe8a92f8a8a9c08.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "f32707ea80e031092f31",
+    "url": "/clicky-game/static/js/main.f32707ea.chunk.js"
+  },
+  {
+    "revision": "8fdf2345b7a4e11badcc",
+    "url": "/clicky-game/static/js/1.8fdf2345.chunk.js"
+  },
+  {
+    "revision": "8ae6b81834d490eef1b1ab90072a1f4b",
+    "url": "/clicky-game/index.html"
+  }
+];
